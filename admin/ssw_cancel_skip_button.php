bcaaacd048c0cb1fd18d964a1f9a226ecd4e37eb
Konain Mukadam <kmm976@nyu.edu>
2018-07-31 15:13:52 -0400

    Accessibility Updates to the SSW
    Changelog:
    1. Changed box colors
    2. Changed Back and Next button colors
    3. Added aria-elemets to radio buttons
    4. Chnanged breadcrumb next symbol from -> to »

--- admin/ssw_cancel_skip_button.php
+++ admin/ssw_cancel_skip_button.php
@@ -2,12 +2,12 @@
 
 echo '
 <div class="ssw-xtra-block">
-  <a href="#" onclick="ssw_js_submit_form_cancel()" style="color:red;" value="Cancel" />Cancel</a>
+  <a href="#" onclick="ssw_js_submit_form_cancel()" value="Cancel" />Cancel</a>
   ';
   if(isset($can_skip)) {
     echo '&nbsp;|&nbsp;<a href="#" onclick="ssw_js_submit_form_skip()">Skip</a>';
   }
-  echo '    	
+  echo '
 </div>
 ';
 
